f4ebf0a68b6d990c43a398279b763c1f86022a14
chandan kumar <chandankumar700451@gamil.com>
2025-01-20 15:51:28 +0530

    replace data local server to online server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-const app = express();
+require('dotenv').config();
+const app = express(); 
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect("mongodb://localhost:27017/MYStudent", { useNewUrlParser: true, useUnifiedTopology: true });
+const port =process.env.port||3001
+//mongoose.connect("mongodb://localhost:27017/MYStudent");
+//mongoose.connect("mongodb+srv://chandankumar700451:chandan700451@cluster0.kvvng.mongodb.net/MYStudent?retryWrites=true&w=majority");
+const mongoURL ="mongodb+srv://chandankumar700451:chandan700451@cluster0.kvvng.mongodb.net/MYStudent?retryWrites=true&w=majority"
+mongoose.connect(mongoURL)
 
 const db = mongoose.connection;
 db.on("error", () => console.log("Error in Connecting to Database"));
@@ -65,6 +68,30 @@ app.post("/sign_up_org", async (req, res) => {
     }   
 });
 
+
+app.put('/:id', async (req, res) => {
+    try {
+        const userId = req.params.id; // Extract the user ID from the URL parameter
+        const updatedUserData = req.body; // Get the updated data from the request body
+
+        const response = await User.findByIdAndUpdate(userId, updatedUserData, {
+            new: true, // Return the updated document
+            runValidators: true // Run validation on the updated data
+        });
+
+        if (!response) { 
+            return res.status(404).json({ error: 'User not found' }); // Handle case where user is not found
+        }
+
+        console.log('User data updated');
+        res.status(200).json(response); // Send the updated user data in the response
+    } catch (err) {
+        console.log('Error updating user:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
 // Home Route
 app.get("/", (req, res) => {
     res.set({ "Access-Control-Allow-Origin": "*" });
@@ -72,6 +99,6 @@ app.get("/", (req, res) => {
 });
 
 // Start Server 
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log("Listening on port 3001");
 });
